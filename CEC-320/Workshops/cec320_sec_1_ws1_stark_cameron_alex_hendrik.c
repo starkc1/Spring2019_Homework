3da3bb601944a05d47e4944387f0189fadfe4878
Cameron Stark <stark@Camerons-Air.attlocal.net>
2019-02-01 14:22:02 -0500

    test

--- CEC-320/Workshops/cec320_sec_1_ws1_stark_cameron_alex_hendrik.c
+++ CEC-320/Workshops/cec320_sec_1_ws1_stark_cameron_alex_hendrik.c
@@ -33,38 +33,45 @@ void print_str_c(uint16_t num, char *cPtr)
 void print_str_v(uint16_t num, char *cPtr_c, char *cPtr_v)
 {
     int j = 1;
+    int shift = 0;
     count = 0;
 
-    for (int i = 0; i < 20; i++) {
-        if (i % 5 == 0) {
-            *(cPtr_v) << '_';
-        } else {
-            *(cPtr_v + i) << *(cPtr_c + i);
-        }
-        
-    }
+    for(int i = 0; i < 30; i++){
+        if(i % 5 == 0){
+            *(cPtr_v + i) = '_';
+            shift++;
+        } 
+        *(cPtr_v + i + shift) = *(cPtr_c + i);
+        printf("I am on number %d\n", i);
+    } //Theoretically it should work, I have no idea why its broken, and my vscode still doesnt see mingw
 
     // for (int i = 0; i <= 20; i++) {
-    //     // j = i % 5;
+    //     j = i % 5;
         
-    //     // if (j == 0 && i != 0) {
-    //     //     *(cPtr_v + count) = '_';
+    //     if (j == 0 && i != 0) {
+    //         *(cPtr_v + count) = '_';
             
-    //     //     *(cPtr_v + count) = *(cPtr_c + i);
+    //         *(cPtr_v + count) = *(cPtr_c + i);
             
-    //     //     //*(cPtr_v + count) = '_';
-    //     //     //*(cPtr_v + count+1) = *(cPtr_c + i+1);
-    //     // } else {
-    //     //     *(cPtr_v + count) = *(cPtr_c + i);
+    //         //*(cPtr_v + count) = '_';
+    //         //*(cPtr_v + count+1) = *(cPtr_c + i+1);
+    //     } else {
+    //         *(cPtr_v + count) = *(cPtr_c + i);
             
-    //     //     //*(cPtr_v + count) = *(cPtr_c + i);
-    //     // }
-    //     // count++;
+    //         //*(cPtr_v + count) = *(cPtr_c + i);
+    //     }
+    //     count++;
         
 
     // }   
+    
+    // for (int i = 0; i < 30; i++) {
+    //     if (i % 5 == 0 && i != 0) {
 
-    printf("%s", cPtr_v);
+    //     }
+    // }
+
+    printf("0b%s", cPtr_v);
 }
 
 int main(void)
@@ -72,7 +79,7 @@ int main(void)
     uint16_t test_num[] = {0x1234, 0x5678, 0x9ABC, 0xDEF0};
     int N = sizeof(test_num) / sizeof(test_num[0]); 
     printf("Testing the printout of binary numbers:\n");
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < 1; i++)
     {
         printf("Compact display for 0x%x is ", test_num[i]);
         print_str_c(test_num[i], str_c);
