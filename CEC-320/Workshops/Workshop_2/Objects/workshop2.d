c7e8b8af56ba0289376677495c840cdd3443912a
Cameron Stark <starkc920@outlook.com>
2019-02-11 17:36:19 -0500

    added new stuff

---
+++ CEC-320/Workshops/Workshop_2/Objects/workshop2.d
@@ -0,0 +1,4 @@
+.\objects\workshop2.o: Workshop2.c
+.\objects\workshop2.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\workshop2.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\workshop2.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
